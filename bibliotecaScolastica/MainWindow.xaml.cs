c829f138dfce9c21b1847d1711d9906df4814105
Enes Sela <ENES.SELA@local.ittcesena.it>
2022-11-17 08:44:42 +0100

    Completato BranchLibro

--- bibliotecaScolastica/MainWindow.xaml.cs
+++ bibliotecaScolastica/MainWindow.xaml.cs
@@ -24,5 +24,47 @@ namespace bibliotecaScolastica
         {
             InitializeComponent();
         }
+
+        class Libro 
+        {
+            internal string Autore { get; set; }
+            internal string Titolo { get; set; }
+            internal string Editore { get; set; }
+            internal int AnnoPubblicazione { get; set; }
+            internal int NumeroPagine { get; set; }
+
+            internal Libro(string autore, string titolo, string editore, int annoPubblicazione, int numeroPagine)
+            {
+                this.Autore = autore;
+                this.Titolo = titolo;
+                this.Editore = editore;
+                this.AnnoPubblicazione = annoPubblicazione;
+                this.NumeroPagine = numeroPagine;
+            }
+
+            public Libro toString(string autore, string titolo, string editore, int annoPubblicazione, int numeroPagine) 
+            {
+                return new Libro(autore, titolo, editore, annoPubblicazione, numeroPagine);
+            }
+
+            public int readingTime(string autore, string titolo, string editore, int annoPubblicazione, int numeroPagine) 
+            {
+                int tempo = 0;
+
+                if (numeroPagine < 100)
+                {
+                    tempo = 60;
+                }
+                else if(numeroPagine > 100 && numeroPagine < 200) 
+                {
+                    tempo = 120;  
+                }
+                else if(numeroPagine > 200) 
+                {
+                    tempo = 180;
+                }
+                return tempo;
+            }
+        }
     }
 }
